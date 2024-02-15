3ee28831d513307a022121925787763f10cabcdb
AESolodchuk <solodchuk.aleksey@gmail.com>
2024-02-15 20:54:38 +0200

    completed

--- task_1.py
+++ task_1.py
@@ -2,5 +2,38 @@ from queue import Queue
 
 queue = Queue()
 
-def menu():
-    
+menu  = ["1. Add new request","2. Pick request from the queue", "0. Exit"]
+
+def generate_request(new_request):
+    request = {hash(new_request):new_request}
+    queue.put(request)
+    return f"\nThe request '{new_request}' was added to the queue. Your unique request number is {request}\n"
+
+def process_request():
+    if not queue.empty():
+        return f"\nRequest {queue.get()} was picked from the queue and assigned to you.\n"
+    else:
+        return "\nQueue is empty\n"
+
+print("Hello, to interact with me, please input command number below:\n")
+while True:
+    for el in menu:
+        print(el)
+    response = input("\nPlease make your choice:")
+    try:
+
+        if int(response) == 1:
+            new_request = input("\nInput your request:")
+            print(generate_request(new_request))
+            continue
+
+        elif int(response) == 2:
+            print(process_request())
+            continue
+
+        elif int(response) == 0:
+            print("Goodbye")
+            break
+
+    except:
+        print("\nPlease make sure that your input is relevant. Applicable numbers in the menu.\n")
---
+++ task_2.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+examples = ["level", "R A dar", "RaceCar", "GoIT"]
+
+def palindrome_check(word):
+    formated_word = word.replace(" ","").lower()
+    dr = deque() 
+
+    for char in formated_word:
+        dr.extendleft(char)
+    
+    return formated_word == "".join(dr,)
+
+for example in examples:
+    print(palindrome_check(example)) 
+
