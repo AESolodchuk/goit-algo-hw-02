aefbae5c5cca2317cb330380c30e47255eec0ada
AESolodchuk <solodchuk.aleksey@gmail.com>
2024-02-21 09:42:31 +0200

    final

--- task_2.py
+++ task_2.py
@@ -1,15 +1,19 @@
 from collections import deque
 
-examples = ["level", "R A dar", "RaceCar", "GoIT","not palindrome"]
+examples = ["level", "R A dar", "RaceCar", "GoIT", "not palindrome"]
+
 
 def palindrome_check(word):
-    formated_word = word.replace(" ","").lower()
-    dr = deque() 
+    formated_word = word.replace(" ", "").lower()
+    dr = deque()
 
     for char in formated_word:
         dr.extendleft(char)
-    
-    return formated_word == "".join(dr,)
+
+    return formated_word == "".join(
+        dr,
+    )
+
 
 for example in examples:
     print(palindrome_check(example))
